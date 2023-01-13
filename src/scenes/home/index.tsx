f1d736bd20cd35f8b59931f8594496bbf1d56447
Burak Sencan <74815003+burak-sencan@users.noreply.github.com>
2023-01-13 21:58:02 +0300

    hero-section-end

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -8,6 +8,8 @@ import HomePageGraphic from '@/assets/HomePageGraphic.png'
 import SponsorRedBull from '@/assets/SponsorRedBull.png'
 import SponsorForbes from '@/assets/SponsorForbes.png'
 import SponsorFortune from '@/assets/SponsorFortune.png'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+import { motion } from 'framer-motion'
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void
@@ -19,22 +21,81 @@ function Home({ setSelectedPage }: Props) {
   return (
     <section id='home' className='gap-16 bg-gray-20 py-10 md:h-full md:pb-0 '>
       {/* IMAGE AND MAIN HEADER */}
-      <div>
+      <motion.div
+        className='mx-auto w-5/6 items-center justify-center md:flex md:h-5/6 '
+        onViewportEnter={() => {
+          setSelectedPage(SelectedPage.Home)
+        }}>
         {/* MAIN HEADER */}
-        <div>
+        <div className='z-10 mt-32 md:basis-3/5'>
           {/* HEADINGS */}
-          <div>
-            <div>
-              <div>
+          <motion.div
+            className='md:-mt-20'
+            initial='hidden'
+            whileInView='visible'
+            viewport={{ once: true, amount: 0.7 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}>
+            <div className='relative'>
+              <div className='before:absolute before:-top-20 before:-left-20  before:z-[-1] before:content-evolvetext'>
                 <img src={HomePageText} alt='home-page-text' />
               </div>
             </div>
-          </div>
+
+            <p className='mt-8 text-sm'>
+              Unrival Gtm. Unparalleled Fifness Classes. World Classes Studios
+              to get the body Shapes That you Dream of... Body Now.
+            </p>
+          </motion.div>
+
+          {/* ACTIONS */}
+          <motion.div
+            className='mt-8 flex items-center gap-8'
+            initial='hidden'
+            whileInView='visible'
+            viewport={{ once: true, amount: 0.7 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}>
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionButton>
+            <AnchorLink
+              className='text-sm font-bold underline hover:text-secondary-500'
+              onClick={() => {
+                setSelectedPage(SelectedPage.ContactUs)
+              }}
+              href={`#${SelectedPage.ContactUs}`}>
+              <p>Learn More</p>
+            </AnchorLink>
+          </motion.div>
         </div>
 
         {/* IMAGE */}
-        <div></div>
-      </div>
+        <div
+          className='flex basis-3/5 justify-center md:z-10
+              md:ml-40 md:mt-16 md:justify-items-end'>
+          <img src={HomePageGraphic} alt='home-pageGraphic' />
+        </div>
+      </motion.div>
+
+      {/* SPONSORS */}
+      {isAboveMediumScreens && (
+        <div className='h-[150px] w-full bg-primary-100 py-10'>
+          <div className='mx-auto w-5/6'>
+            <div className='flex w-3/5 items-center justify-between gap-8'>
+              <img src={SponsorRedBull} alt='redbull-sponsor' />
+              <img src={SponsorForbes} alt='forbes-sponsor' />
+              <img src={SponsorFortune} alt='fortune-sponsor' />
+            </div>
+          </div>
+        </div>
+      )}
     </section>
   )
 }
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -23,7 +23,7 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
   return (
     <nav>
       <div
-        className={`${navbarBackgrond} flex-between fixed top-0 z-10 w-full  py-6`}>
+        className={`${navbarBackgrond} flex-between fixed top-0 z-20 w-full  py-6`}>
         <div className='flex-between  mx-auto w-5/6'>
           <div className='flex-between w-full gap-16'>
             {/* left */}
